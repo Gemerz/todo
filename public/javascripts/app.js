6f2dbfef07bc0ee8607a9bf4031dab2dd7fe84b6
Gemer <gemer@wizmacau.com>
2015-07-02 18:54:56 +0800

    temp save

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,7 +2,6 @@ var React = require('react');
 global.React = React;
 
 var TodoApp = require('./components/todo_app');
-var LoginAPP = require('./components/Login');
-
+//var LoginAPP = require('./components/login');
 React.renderComponent(TodoApp(), document.getElementById('todoapp'));
-React.renderComponent(LoginAPP(), document.getElementById('login'));
+//React.renderComponent(LoginAPP(), document.getElementById('login'));
